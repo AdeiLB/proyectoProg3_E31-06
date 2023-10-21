e268e453b8455c7d0ccdde8189d3bfde36f58bc7
Victor Redondo Mula <victor.redondo@opendeusto.es>
2023-10-21 15:48:40 +0200

    Prueba sobre el manejo de git.

--- AstroLone/src/HolaMundo.java
+++ AstroLone/src/HolaMundo.java
@@ -2,5 +2,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("holamundo");
+		System.out.println("Hola AdeiLB este es mi primer commmit desde mi ordenador ");
 	}
 }
